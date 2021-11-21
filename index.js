398b219b6439eca025cbb176907260c0bc668774
Nabeil <nabeillemar@gmail.com>
2021-11-21 12:00:50 -0800

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
 // Write your solution in this file!
+
+var customerName = "bob";
+
+const leastFavoriteCustomer = "test";
+
+function upperCaseCustomerName() {
+    customerName = customerName.toUpperCase()
+  }
+
+
+  function setBestCustomer() {
+      bestCustomer = "not bob"
+  }
+
+  function overwriteBestCustomer(input) {
+    bestCustomer = input
+  }
+  
+  function changeLeastFavoriteCustomer(input) {
+    leastFavoriteCustomer = input
+  } 
